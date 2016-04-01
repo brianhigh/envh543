95daaece6b2dc40aa06d869d37dbc2e2284fa993
Brian High <bhigh@live.com>
2016-04-01 11:24:14 -0700

    Added R scripts for example 6.18.

---
+++ samples/ex0618/ex0618prob2d.R
@@ -0,0 +1,262 @@
+# A 2-D Monte Carlo simulation for the daily microbial exposure from drinking
+# water consumption, swimming in surface water, and shellfish consumption
+# for Example 6.18 from Quantitative Microbial Risk Assessment, 2nd Edition
+# by Charles N. Haas, Joan B. Rose, and Charles P. Gerba. (Wiley, 2014).
+
+# Copyright (c) Jane Pouzou
+# License: CC BY-SA 4.0 - https://creativecommons.org/licenses/by-sa/4.0/
+
+# ---------------------------------------------------------------------
+# Perform a 2-D Monte Carlo simulation
+# ---------------------------------------------------------------------
+
+# ----------------
+# Clear workspace
+# ----------------
+
+# Clear the workspace, unless you are running in knitr context.
+if (!isTRUE(getOption('knitr.in.progress'))) {
+    closeAllConnections()
+    rm(list = ls())
+}
+
+# ------------
+# Set options
+# ------------
+
+# Set display options for use with the print() function.
+options(digits = 5)
+
+# -----------------
+# Define variables
+# -----------------
+
+# Define the deterministic factors.
+shellfish.viral.load <- 1  # Shellfish viral loading (viruses/g)
+dw.viral.load <- 0.001     # Drinking water viral loading (viruses/L)
+shellfish.cons.g <- 0.135  # Shellfish consumption (viruses/day)
+sw.viral.load <- 0.1       # Swimming in surface water viral loading (viruses/L)
+sw.frequency <- 7          # Swimming frequency (swims/year)
+
+# Define an integer to use when setting the seed of the random number generator.
+seed <- 1
+
+# Define an empty matrix to hold the simulation results.
+Risk.mat <- matrix(as.numeric(NA), nrow = 5000, ncol = 250)
+
+# -----------------
+# Define functions
+# -----------------
+
+# Define a function to calculate estimated exposure risk.
+Risk.fcn <- function(shellfish.vl, shellfish.cons.g, water.cons.L, dw.vl, 
+                     sw.vl, sw.daily.IR, sw.duration, sw.frequency) {
+    ((shellfish.vl * shellfish.cons.g) + (water.cons.L * dw.vl) + 
+         ((sw.vl * (sw.daily.IR * sw.duration * sw.frequency)) / 365 / 1000))
+}
+
+# ------------------------
+# Run a 2-D MC simulation
+# ------------------------
+
+# Generate 250 random samples from a normal distribution to estimate 
+# ingestion rate (IR) in mL of surface water. The standard deviation 
+# value of 45 used here is a fictitious example.
+set.seed(seed)
+sw.d.IR <- rnorm(250, mean = 50, sd = 45)
+
+# Plot the kernel density estimates for surface water.
+plot(density(sw.d.IR))
+
+# Run 250 iterations of a 5000-sample simulation.
+for (i in 1:250) {
+    # Generate 5000 random samples from a log-normal distribution to estimate 
+    # exposure from consumption of drinking water (ml/day). Divide by 1000 
+    # mL/L to get consumption in liters/day.  Values for meanlog and sdlog 
+    # are from the QMRA textbook (Haas et al. 2014), page 216, Table 6.30.
+    set.seed(seed)
+    water.cons.L <- rlnorm(n = 5000, meanlog = 7.49, sdlog = 0.407) / 1000
+    
+    # Plot the kernel density estimates for drinking water.
+    #plot(density(water.cons.L))  # Uncomment this if you want to see these.
+    
+    # Sample 5000 times from a discrete distribution of swim duration with 
+    # assigned probabilities of each outcome. These values are hypothetical 
+    # and are not found in the text, but are defined here to provide an 
+    # example of sampling from a discrete distribution.
+    set.seed(seed)
+    swim.duration <- sample(x = c(0.5, 1, 2, 2.6), size = 5000, 
+                            replace = TRUE, prob = c(0.1, 0.1, 0.2, 0.6))
+    
+    # Compute 5000 daily dose simulations and store as a vector in a matrix.
+    Risk.mat[,i] <- sapply(1:5000, function(j) 
+        # Define a function to calculate esimated microbial exposure risk.
+        Risk.fcn(water.cons.L = water.cons.L[j],
+                 sw.duration = swim.duration[j],
+                 shellfish.vl = shellfish.viral.load,
+                 dw.vl = dw.viral.load,
+                 shellfish.cons.g = shellfish.cons.g,
+                 sw.vl = sw.viral.load,
+                 sw.daily.IR = sw.d.IR[i],
+                 sw.frequency = sw.frequency))
+}
+
+# ------------------
+# Summarize results
+# ------------------
+
+# Plot the empirical cumulative distribution for the first iteration.
+plot(ecdf(Risk.mat[, 1]), col = "#ADD8E605", main = 'ecdf(Risk.mat)')
+
+# Plot empirical cumulative distributions for additional iterations in blue.
+for (j in 1:250) {
+    plot(ecdf(Risk.mat[, j]), col = "#ADD8E605", add = TRUE)
+}
+
+# ---------------------------------------------------------------------
+# Repeat the simulation with mc2d
+# ---------------------------------------------------------------------
+
+# There are multiple ways to run the 2-D simulation depending on the 
+# desired output. We will use mc() and mcmodelcut() from the mc2d package.
+
+# --------------
+# Load packages
+# --------------
+
+# Define a function to conditionally install and load a package.
+load.pkg <- function(pkg) {
+    if (! suppressWarnings(require(pkg, character.only = TRUE)) ) {
+        install.packages(pkg, repos = 'http://cran.r-project.org')
+        library(pkg, character.only = TRUE, quietly = TRUE)
+    }
+}
+
+# Load required packages, installing first if necessary.
+suppressMessages(load.pkg("mc2d"))
+suppressMessages(load.pkg("PBSmodelling"))  # For unpackList()
+# Or use library() and take your chances...
+
+# -----------------
+# Define variables
+# -----------------
+
+# Define an integer to use when setting the seed of the random number generator.
+seed <- 1
+
+# Set the number of simulations in the variability dimension.
+ndvar(5000)
+
+# Set the number of simulations in the uncertainty dimension. 
+ndunc(250)
+
+# --------------------
+# Define MC functions
+# --------------------
+
+# Define a function to create mcnode objects for use by mc() and mcmodelcut().
+create_mcnode_objects <- function() {
+    # Values from Example 6.18 from Quantitative Microbial Risk Assessment, 
+    # 2nd Edition by Charles N. Haas, Joan B. Rose, and Charles P. Gerba. 
+    # (Wiley, 2014), pp. 215-216. Other fictitious values are noted below.
+    
+    # Return a list of mcnode objects. (V = variability, U = uncertainty)
+    return(list(
+        # Shellfish viral loading (viruses/g):
+        shellfish.vl = mcstoc(runif, type = "V", min = 1, max = 1),
+        
+        # Shellfish consumption (g/day):
+        shellfish.cons.g = mcstoc(runif, type = "V", min = 0.135, max = 0.135),
+        
+        # Drinking water viral loading (viruses/L):
+        dw.vl = mcstoc(runif, type = "V", min = 0.001, max = 0.001),
+        
+        # Drinking water consumption (L/day):
+        dw.cons.L = mcstoc(rlnorm, type = "V", meanlog = 7.49, sdlog = 0.407, 
+                           seed = seed) / 1000,
+        
+        # Swimming in surface water viral loading (viruses/L):
+        sw.vl = mcstoc(runif, type = "V", min = 0.1, max = 0.1),
+        
+        # Swimming daily ingestion rate (mL/hour): fictitious sd = 45
+        sw.daily.IR = mcstoc(rnorm, type = "U", mean = 50, sd = 45, 
+                             seed = seed, rtrunc = TRUE, linf = 0),
+        
+        # Swimming duration (hours): fictitious discrete distribution
+        sw.duration = mcstoc(rempiricalD, type = "V", 
+                             values = c(0.5, 1, 2, 2.6), 
+                             prob = c(0.1, 0.1, 0.2, 0.6)),
+        
+        # Swimming frequency (swims/year):
+        sw.frequency = mcstoc(runif, type = "V", min = 7, max = 7)))
+}
+
+# Define a function to estimate exposure risk from mcnode objects.
+calc_dose <- function(mcnodes) {
+    with(mcnodes, ((shellfish.vl * shellfish.cons.g) + (dw.vl * dw.cons.L) +
+                       ((sw.vl * (sw.daily.IR * sw.duration * sw.frequency)) / 365 / 1000)))
+}
+
+# ------------------------
+# Run a 1-D MC simulation
+# ------------------------
+
+# Create a Monte Carlo object from a set of mcnode objects.
+dose1 <- mc(calc_dose(create_mcnode_objects()))
+
+# Plot the Monte Carlo object.
+plot(dose1)
+
+# ------------------------
+# Run a 2-D MC simulation
+# ------------------------
+
+# Get a character vector of mcnode names to use in Block 2 of mcmodelcut().
+mcnode.names <- names(create_mcnode_objects())
+
+# Build a mcmodelcut object for evaluation by evalmccut().
+dosemccut <- mcmodelcut({
+    # Evaluate a 2-D MC model using a loop to gather statistics.
+    
+    # Block 1: Evaluate all of the 0, V and U mcnodes. (Runs only once.)
+{
+    # Create the mcnode objects and and extract them from a list.
+    unpackList(create_mcnode_objects())
+}
+
+# Block2: Evaluate all of the VU nodes. Loop on the uncertainty dimension.
+{
+    # Estimate the exposure using the V and U nodes to create a VU node.
+    dose2 <- calc_dose(mget(mcnode.names))
+    
+    # Note: Generates a warning from using do.call(), which can be ignored.
+    dose.model <- do.call(mc, mget(c(mcnode.names, "dose2"))) 
+}
+
+# Block 3: Calculate statistics in the variability dimension (within loop).
+{
+    list(sum = summary(dose.model), plot = plot(dose.model, draw = FALSE))
+}
+
+})
+
+# Evaluate the 2-D Monte Carlo model using a loop on the uncertainty dimension.
+# Capture the text output and print when finished to save space in the report.
+capture.output(x <- evalmccut(dosemccut, nsv = 5000, nsu = 250, seed = seed))
+
+# ------------------
+# Summarize results
+# ------------------
+
+# Print a summary and a plot for the mccut object.
+summary(x)
+plot(x)
+
+# Plot the empirical cumulative distribution for the estimated exposure.
+expo.x <- x$plot$dose2
+expo.l <- length(expo.x)
+expo.y <- 1:expo.l/expo.l
+plot(expo.x, expo.y, pch = 20, cex = 0.1, col = '#ADD8E604', 
+     main='ecdf(dose2)', ylab = 'Fn(x)', xlab = 'x')
+abline(h = 0, col = "gray", lty = 2, lwd = 2)
+abline(h = 1, col = "gray", lty = 2, lwd = 2)
